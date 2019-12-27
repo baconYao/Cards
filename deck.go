16c51cf8ee524019f1d6f50b89ad2472f57dffc7
peiyao <peiyaochang@qnap.com>
2019-12-27 11:42:31 +0800

    added newDeck() method to create cards

--- deck.go
+++ deck.go
@@ -6,6 +6,21 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 // Any variable type of "deck" now gets access to the "print" method
 func (d deck) print() {
 	for i, card := range d {
