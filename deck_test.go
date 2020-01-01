f8c95699170d9b0ec82717d520ac82557fc09e5e
baconYao <bacon735392@gmail.com>
2020-01-01 09:53:33 +0800

    added TestNewDeck test case to verify newDeck()'s functionality

--- deck_test.go
+++ deck_test.go
@@ -1,2 +1,21 @@
 package main
 
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+	// Target: Code to make sure that a deck is created with x number of cards
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 20, but got: %v", len(d))
+	}
+
+	// Target: Code to make sure that the first card is an Ace of Spades
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card is Ace of Spades, but got %v", d[0])
+	}
+
+	// Target: Code to make sure that the last card is a Four of Clubs
+	if d[len(d) - 1] != "Four of Clubs" {
+		t.Errorf("Expected last card is Four of Clubs, but got %v", d[len(d)-1])
+	}
+}
